4fd85c7233334b480f8495536d7b1c10ae009c99
charan <charan932001@gmail.com>
2024-07-02 18:33:30 +0530

    gridadded

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Grid from '@mui/material/Grid';
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Welcome to Dashboard</h1>
-    </div>
-    
+    <div className='container'>
+      <div style={{display:'flex',gridColumnGap:'10px'}}>
+      <Grid style={{height:'251px',width:'704px',backgroundColor:'#21222D',borderRadius:'10px'}}>item1</Grid>
+      <Grid style={{height:'251px',width:'438px',backgroundColor:'#21222D',borderRadius:'10px'}}>item2</Grid>
+      </div>
+      <div style={{display:'flex',gridColumnGap:'10px',marginTop:'10px'}}>
+      <Grid style={{height:'251px',width:'704px',backgroundColor:'#21222D',borderRadius:'10px'}}>item3</Grid>
+      <Grid style={{height:'251px',width:'438px',backgroundColor:'#21222D',borderRadius:'10px'}}>item4</Grid>
+      </div>
+      <div style={{display:'flex',gridColumnGap:'10px',marginTop:'10px'}}>
+      <Grid style={{height:'251px',width:'438px',backgroundColor:'#21222D',borderRadius:'10px'}}>item5</Grid>
+      <Grid style={{height:'251px',width:'704px',backgroundColor:'#21222D',borderRadius:'10px'}}>item6</Grid>
+      </div>
+     </div>
   )
 }
+
 export default Dashboard
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='header'>
-      <input style={{position: 'absolute',top: '17px',left: '175px',width: '504px',height: '44px',backgroundColor: '#21222D',borderRadius: '8px',borderStyle: 'none',paddingLeft: '35px',color: '#D2D2D2',fontFamily: 'Inter',fontWeight: '500',fontSize: '12px',lineHeight: '14.52px'}} type='text' placeholder='Search here...'/>
-      <img style={{width: '10.83px',height:'10.83px',position:'relative',left:'190px',bottom:'1px',backgroundColor: '#21222D'}}src='search.png'alt='search'/>
+      <input style={{position: 'absolute',top: '17px',left: '210px',width: '504px',height: '44px',backgroundColor: '#21222D',borderRadius: '8px',borderStyle: 'none',paddingLeft: '35px',color: '#D2D2D2',fontFamily: 'Inter',fontWeight: '500',fontSize: '12px',lineHeight: '14.52px'}} type='text' placeholder='Search here...'/>
+      <img style={{width: '10.83px',height:'10.83px',position:'relative',left:'230px',bottom:'1px',backgroundColor: '#21222D'}}src='search.png'alt='search'/>
       <div className='pic'>
-      <img style={{  width: '14.89px',height: '16.79px'}} src='notification.png' alt='notification' />
+      <img style={{ width: '14.89px',height: '16.79px'}} src='notification.png' alt='notification' />
       <img style={{width: '6px',height: '6px',position: 'relative',bottom: '9px',right: '6px'}} src='Ellipse.png' alt='dot' />
       <img style={{marginRight: '15px',marginLeft:'15px',height:'29px',width:'29px'}}src='man.png' alt='man'/>
       <img style={{height:'12px',width:'6px',transform:'rotate(360deg)'}} src='dropdowm.png' height={'12px'} width={'6px'} alt='dropdown'/>
