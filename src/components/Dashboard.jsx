fe6a7e4d74e8e98d62ac9935770a27d531871e93
charan <charan932001@gmail.com>
2024-07-07 10:57:03 +0530

    newchanges

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend, ResponsiveContainer } from 'recharts';
+import { Line } from 'react-chartjs-2';
+import {Chart as ChartJS,CategoryScale,LinearScale,PointElement,LineElement,Title,Tooltip,Filler,} from 'chart.js';
+import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
+import { LineChart, lineElementClasses } from '@mui/x-charts/LineChart';
+ChartJS.register(CategoryScale,LinearScale,PointElement,LineElement,Title,Tooltip,Filler);
 const Dashboard = () => {
   const data = [
     { name: 'A', volume: 300, service: 200 },
@@ -11,24 +15,74 @@ const Dashboard = () => {
     { name: 'D', volume: 280, service: 230 },
     { name: 'E', volume: 290, service: 180 },
   ];
-  const products = [
-    { id: 1, name: 'Home Decore Range', popularity: 'high', sales: 46 },
-    { id: 2, name: 'Disney Princess Dress', popularity: 'medium', sales: 17 },
-    { id: 3, name: 'Bathroom Essentials', popularity: 'medium', sales: 19 },
-    { id: 4, name: 'Apple Smartwatch', popularity: 'low', sales: 29 },
+  const products=[
+      { id: 1, name: 'Home Decore Range', popularity: 80, sales: 46, salesColor: '#FFAC42' },
+      { id: 2, name: 'Disney Princess Dress', popularity: 60, sales: 17, salesColor: '#88E6E5' },
+      { id: 3, name: 'Bathroom Essentials', popularity: 70, sales: 19, salesColor: '#209BF7' },
+      { id: 4, name: 'Apple Smartwatch', popularity: 40, sales: 29, salesColor: '#D7ACE7' },
+    ];
+    const getPopularityColor = (popularity) => {
+      if (popularity > 70) return '#FFAC42'; 
+      if (popularity > 40) return '#88E6E5'; 
+      return '#209BF7';
+    };
+    const graphdata = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Dataset 1',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: true,
+          backgroundColor: 'rgba(75,192,192,0.2)',
+          borderColor: 'rgba(75,192,192,1)',
+         
+        },
+        {
+          label: 'Dataset 2',
+          data: [28, 48, 40, 19, 86, 27, 90],
+          fill: true,
+          backgroundColor: 'rgba(153,102,255,0.2)',
+          borderColor: 'rgba(153,102,255,1)',
+        },
+      ],
+    };
+  
+    const options = {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          enabled: false,
+        },
+      },
+      scales: {
+        y: {
+          display: false,
+          grid: {
+            display: false,
+          },
+        },
+        x: {
+          display: false,
+          grid: {
+            display: false,
+          },
+        },
+      },
+    };
+    const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+    const xLabels = [
+     'Jan',
+     'Feb',
+     'Mar',
+     'Apr',
+     'May',
+     'Jun',
+     'Jul',
   ];
-  const getPopularityClass = (popularity) => {
-    switch (popularity) {
-      case 'high':
-        return 'popularity-high';
-      case 'medium':
-        return 'popularity-medium';
-      case 'low':
-        return 'popularity-low';
-      default:
-        return '';
-    }
-  };
   return (
     <div className='container'>
       <div style={{display:'flex',gridColumnGap:'10px'}}>
@@ -56,7 +110,7 @@ const Dashboard = () => {
         </Grid>
       <Grid className='innergrid'>
       <div style={{marginLeft:'12px',marginTop:'10px'}}>
-        <img className='gridimage' src='none2.png' alt='icon3'/>
+        <img className='gridimage' src='none4.png' alt='icon3'/>
         <Typography style={{height:'18px',width:'28px',fontFamily:'Inter',fontWeight:'600',fontSize:'15px',lineHeight:'18.15px',color:'#ffff',marginTop:'12px'}}>9</Typography>
         <Typography style={{height:'12px',width:'54px',fontFamily:'Inter',fontWeight:'500',fontSize:'10px',lineHeight:'12.1px',color:'#ffff',marginTop:'5px'}}>Product Sold</Typography>
         <Typography style={{height:'10px',width:'81px',fontFamily:'Inter',fontWeight:'500',fontSize:'8px',lineHeight:'9.68px',color:'#F2C8ED',marginTop:'5px'}}>+2% from yesterday</Typography>
@@ -90,41 +144,85 @@ const Dashboard = () => {
       <Grid style={{height:'251px',width:'704px',backgroundColor:'#21222D',borderRadius:'10px'}}>
         <div style={{marginLeft:'14px',marginTop:'14px'}}>
         <Typography style={{width:'97px',height:'18px',fontFamily:'Inter',fontWeight:'600',fontSize:'15px',lineHeight:'18.15px',color:'#ffff'}}>Top Products</Typography>
-        <div className="top-products">
-      <table>
-        <thead>
-          <tr>
-            <th><Typography className='headertext'>#</Typography></th>
-            <th><Typography className='headertext'>Name</Typography></th>
-            <th><Typography className='headertext'>Popularity</Typography></th>
-            <th><Typography className='headertext'>Sales</Typography></th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <td>{product.id < 10 ? `0${product.id}` : product.id}</td>
-              <td>{product.name}</td>
-              <td>
-                <div className={`popularity-bar ${getPopularityClass(product.popularity)}`}></div>
-              </td>
-              <td>{product.sales}%</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+        <table className="top-products">
+      <thead>
+        <tr>
+          <th><Typography style={{width:'9px',height:'16px',fontFamily:'Inter',fontWeight:'500',fontSize:'13px',lineHeight:'15.73px',color: '#87888C'}}>#</Typography></th>
+          <th><Typography style={{width:'37px',height:'16px',fontFamily:'Inter',fontWeight:'500',fontSize:'13px',lineHeight:'15.73px',color: '#87888C'}}>Name</Typography></th>
+          <th><Typography style={{width:'63px',height:'16px',fontFamily:'Inter',fontWeight:'500',fontSize:'13px',lineHeight:'15.73px',color: '#87888C'}}>Popularity</Typography></th>
+          <th><Typography style={{width:'34px',height:'16px',fontFamily:'Inter',fontWeight:'500',fontSize:'13px',lineHeight:'15.73px',color: '#87888C'}}>Sales</Typography></th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+           <tr key={product.id}>
+           <td style={{width:'15px',height:'16px',fontFamily:'Inter',fontWeight:'500',fontSize:'13px',lineHeight:'15.73px'}}>{product.id < 10 ? `0${product.id}` : product.id}</td>
+           <td style={{width:'107px',height:'12px',fontFamily:'Inter',fontWeight:'500',fontSize:'10px',lineHeight:'15.73px'}}>{product.name}</td>
+           <td>
+             <div className="popularity-bar-container">
+               <div
+                 className="popularity-bar"
+                 style={{ width: `${product.popularity}%`, backgroundColor: getPopularityColor(product.popularity) }}
+               ></div>
+             </div>
+           </td>
+           <td>
+             <span className="sales-icon" style={{ backgroundColor: product.salesColor }}>
+               {product.sales}%
+             </span>
+           </td>
+         </tr>
+          
+        ))}
+      </tbody>
+    </table>
         </div>
-
       </Grid>
-      <Grid style={{height:'251px',width:'438px',backgroundColor:'#21222D',borderRadius:'10px'}}>item4</Grid>
+      <Grid style={{height:'251px',width:'438px',backgroundColor:'#21222D',borderRadius:'10px'}}>
+        <div style={{marginLeft:'14px',marginTop:'14px'}}>
+        <Typography style={{width:'147.9px',height:'18px',fontFamily:'Inter',fontWeight:'600',fontSize:'15px',lineHeight:'18.15px'}}>Customer Fulfilment</Typography>
+        <div style={{ width: '400px',height:'190px', borderRadius: '10px' }}>
+         <Line data={graphdata} options={options} />
+        </div>
+        </div>
+      </Grid>
       </div>
       <div style={{display:'flex',gridColumnGap:'10px',marginTop:'10px'}}>
-      <Grid style={{height:'251px',width:'438px',backgroundColor:'#21222D',borderRadius:'10px'}}>item5</Grid>
-      <Grid style={{height:'251px',width:'704px',backgroundColor:'#21222D',borderRadius:'10px'}}>item6</Grid>
+      <Grid style={{height:'251px',width:'438px',backgroundColor:'#21222D',borderRadius:'10px'}}>
+        <div style={{marginLeft:'14px',marginTop:'14px'}}>
+          <Typography style={{width:'64px',height:'18px',fontFamily:'Inter',fontWeight:'600',fontSize:'15px',lineHeight:'18.15px',color:'#fffff'}}>Earnings</Typography>
+          <Typography style={{width:'68px',height:'12px',fontFamily:'Inter',fontWeight:'500',fontSize:'10px',lineHeight:'12.1px',color:'#fffff',marginTop:'5px'}}>Total Expense</Typography>
+          <Typography style={{width:'95px',height:'24px',fontFamily:'Inter',fontWeight:'700',fontSize:'20px',lineHeight:'24.2px',color:'#fffff',marginTop:'5px'}}>$6078.8</Typography>
+          <Typography style={{width:'151px',height:'26px',fontFamily:'Inter',fontWeight:'500',fontSize:'11px',lineHeight:'13.31px',color:'#87888C',marginTop:'5px'}}>profit is $48% More than last Month</Typography>
+          <div style={{height:'140px',Width:'140px'}}>
+          <Gauge value={80} startAngle={-110} endAngle={110} sx={{[`& .${gaugeClasses.valueText}`]: {fontSize: 35,transform: 'translate(0px, 0px)',color:'white'},[`& .${gaugeClasses.valueArc}`]: {fill: '#A9DFD8'}}}
+          text={
+             ({ value }) => `${value}%`
+           }/>
+          </div>
+        </div>
+      </Grid>
+      <Grid style={{height:'251px',width:'704px',backgroundColor:'#21222D',borderRadius:'10px'}}>
+        <div style={{marginTop:'14px',marginLeft:'14px'}}>
+        <Typography style={{width:'110px',height:'18px',fontFamily:'Inter',fontWeight:'600',fontSize:'15px',lineHeight:'18.15px',color:'#fffff'}}>Visitors Insights</Typography>
+        <LineChart
+      width={700}
+      height={240}
+      series={[{ data: uData, area: true, showMark: false,color:'#A9DFD8' }]}
+      xAxis={[{ scaleType: 'point', data: xLabels }]}
+      sx={{
+        [`& .${lineElementClasses.root}`]: {
+          display: 'none',
+        },
+        Color: 'blue',
+      }}
+    />
+        </div>
+      </Grid>
       </div>
      </div>
   )
+
 }
 
 export default Dashboard
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,22 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 const Header = () => {
   return (
     <div className='header'>
+      <Typography style={{height:'15px',width:'150px',position:'absolute',top:'5px',left:'38px',fontFamily:'Inter',fontWeight:'500',fontSize:'10px'}}>G3 Innovations</Typography>
+      <img style={{position:'absolute',left:'50px',height:'50px',width:'50px',borderRadius:'23px'}} src='g3image.jpeg' alt='g3'/>
       <input style={{position: 'absolute',top: '17px',left: '210px',width: '504px',height: '44px',backgroundColor: '#21222D',borderRadius: '8px',borderStyle: 'none',paddingLeft: '35px',color: '#D2D2D2',fontFamily: 'Inter',fontWeight: '500',fontSize: '12px',lineHeight: '14.52px'}} type='text' placeholder='Search here...'/>
       <img style={{width: '10.83px',height:'10.83px',position:'relative',left:'230px',bottom:'1px',backgroundColor: '#21222D'}}src='search.png'alt='search'/>
       <div className='pic'>
       <img style={{ width: '14.89px',height: '16.79px'}} src='notification.png' alt='notification' />
       <img style={{width: '6px',height: '6px',position: 'relative',bottom: '9px',right: '6px'}} src='Ellipse.png' alt='dot' />
       <img style={{marginRight: '15px',marginLeft:'15px',height:'29px',width:'29px'}}src='man.png' alt='man'/>
-      <img style={{height:'12px',width:'6px',transform:'rotate(360deg)'}} src='dropdowm.png' height={'12px'} width={'6px'} alt='dropdown'/>
+      <select style={{height:'15px',width:'15px',color:'white',backgroundColor:'#21222D',borderStyle:'none'}}>
+        <option>profile</option>
+        <option>settings</option>
+        <option>logout</option>
+      </select>
       </div>
     </div>
   )
